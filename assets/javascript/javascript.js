f59ced399c1f6f39b9daf98cfe042a4874739461
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-16 15:39:03 -0500

    adding base html and js code

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+
+// this still needs to be altered to meet requirements, currently just placeholder with correct API key
+var character = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10"
+
+
+
+});
