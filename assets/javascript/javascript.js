efa9a5bd47d01a27c016245d2ef1e9b8a9f45891
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-16 16:19:41 -0500

    adding click function

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,34 +1,40 @@
 $(document).ready(function () {
 
 
-var character = "";
 
-var api_key = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10";
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + api_key;
 
-var topics = 
-    ["Ron Swanson", 
-    "Jared Dunn",
-    "Tina", 
-    "Cartman", 
-    "Blake Anderson", 
-    "Jessica Jones"];
+    var character = "";
+
+    var api_key = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + api_key;
+
+    var topics =
+        ["Ron Swanson",
+            "Jared Dunn",
+            "Tina",
+            "Cartman",
+            "Blake Anderson",
+            "Jessica Jones"];
 
     for (var i = 0; i < topics.length; i++) {
-        var buttons = $('<button>'+ topics[i] + '</button>') 
-        buttons.appendTo('#buttons'); 
+        var buttons = $('<button>' + topics[i] + '</button>')
+        buttons.appendTo('#buttons');
     }
+    $('button').on('click', function () {
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      .then(function(response){
-          var results = response.data;
 
-          console.log(response);
-      })
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
 
-      console.log(queryURL);
+                console.log(response);
+            })
+
+    })
+
+    console.log(queryURL);
 
 });
