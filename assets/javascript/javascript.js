bf8e1d0f3139e1cd425445c9080a5e0d96e8da2f
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-16 15:48:54 -0500

    fixing jQuery cdn

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,24 @@
 $(document).ready(function () {
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
 
-// this still needs to be altered to meet requirements, currently just placeholder with correct API key
-var character = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10"
+var character = "";
 
+var api_key = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + api_key;
 
+var topics = 
+    ["Ron Swanson", 
+    "Jared Dunn",
+    "Tina", 
+    "Cartman", 
+    "Blake Anderson", 
+    "Jessica Jones"];
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+      console.log(queryURL);
 
 });
