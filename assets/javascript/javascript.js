4ac2f20324b30f2d0d403ca38351427cdf0c09ee
KenzieAnnC <35305283+KenzieAnnC@users.noreply.github.com>
2018-04-17 15:38:45 -0500

    fixing response

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,60 +13,59 @@ $(document).ready(function () {
             "Jessica Jones"];
 
 
-
+    // this loop (is supposed to) iterate through all the topics an assign the variable character to the data-character value of each button
 
     for (var i = 0; i < topics.length; i++) {
         $('<button/>', {
             text: topics[i],
             'data-character': topics[i]
         })
-            // $('<button data-character>' + topics[i] + '</button>');
-
             .appendTo('#buttons');
+
         var character = $('button').attr('data-character');
-        // console.log('this ' + characater);
-    }
+        // console.log(character);
 
 
 
+        $(document).on('click', 'button', function() {
+            var api_key = "&api_key=fU89AfoeG5J5kBkCYMh5BiL4ZqQGzBzW&limit=10";
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + character + api_key;
+        
 
-    $(document).on('click', 'button', function () {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+                .then(function (response) {
+                    var results = response.data;
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-            .then(function (response) {
-                var results = response.data;
+                    console.log(response.data);
 
-                console.log(response.data);
+                    for (var i = 0; i < results.length; i++) {
 
-                // console.log(response);
-                for (var i = 0; i < results.length; i++) {
+                        if (results[i].rating !== "r") {
 
-                    if (results[i].rating !== "r") {
+                            var gifDiv = $("<div class= 'item'>");
 
-                        var gifDiv = $("<div class= 'item'>");
+                            var rating = results[i].rating;
 
-                        var rating = results[i].rating;
+                            var p = $('<p>');
 
-                        var p = $('<p>');
+                            var characterImage = $('<img>');
+                            characterImage.attr('src', results[i].images.fixed_height.url);
 
-                        var characterImage = $('<img>');
-                        characterImage.attr('src', results[i].images.fixed_height.url);
+                            gifDiv.append(p);
+                            gifDiv.append(characterImage);
 
-                        gifDiv.append(p);
-                        gifDiv.append(characterImage);
+                            $("#gifs-appear-here").prepend(gifDiv);
 
-                        $("#gifs-appear-here").prepend(gifDiv);
+                        }
 
                     }
+                });
 
-                }
-            });
+        });
 
-    });
-
-
+    }
 
 });
